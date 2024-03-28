4022a907f2be9dd4a3ae5bf2f3551aab48d1e49d
vdmkkk <soloviov.vad@gmail.com>
2024-03-28 10:56:05 +0300

    two sidebars with some logic

--- src/components/Main-Sidebar/Main-Sidebar.js
+++ src/components/Main-Sidebar/Main-Sidebar.js
@@ -3,7 +3,7 @@ import './Main-Sidebar.scss'
 function MainSidebar({open}) {
     return(
         <div className={open ? "main-sidebar sidebar-open" : "main-sidebar sidebar-closed"}>
-
+            <p>Привет, мир!</p>
         </div>
     )
 }
