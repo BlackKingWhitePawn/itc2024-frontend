a4f11a5d6de69064c58db75689d2c07ae658a583
AlexGorin <alexgorin37@gmail.com>
2024-03-28 13:11:58 +0500

    Deleted unneccasary sidebar

--- src/pages/analys-page.jsx
+++ src/pages/analys-page.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import userStore from '../stores/user'
 import { useNavigate } from 'react-router-dom'
 import Dashboard from '../components/Dashboard'
-import { SideBar } from '../components/reusable/SideBar'
 
 
 function AnalysPage() {
