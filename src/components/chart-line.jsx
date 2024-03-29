3c9e0e47ae7d360232d0c63b7c6b2a5e2d491667
AlexxxGorin <alexgorin37@gmail.com>
2024-03-29 10:19:12 +0500

    Solved line-chart

--- src/components/chart-line.jsx
+++ src/components/chart-line.jsx
@@ -3,16 +3,24 @@ import { LineChart } from '@mui/x-charts/LineChart';
 import { Typography } from '@mui/material';
 
 
-/**
- * Renders a line chart based on the provided data.
- *
- * @param {Object} data - The data object containing the chart data.
- * @param {Array} data.x - The array of x-axis values.
- * @param {Array} data.y - The array of y-axis values.
- * @param {Array} data.ys - The array of arrays representing multiple sets of y-axis values.
- * @example
- * ```
- * const data = {
+
+
+// /**
+//  * Renders a line chart based on the provided data.
+//  *
+//  * @param {Object} data - The data object containing the chart data.
+//  * @param {Array} data.x - The array of x-axis values.
+//  * @param {Array} data.y - The array of y-axis values.
+//  * @param {Array} data.ys - The array of arrays representing multiple sets of y-axis values.
+//  * @example
+//  * ```
+
+//     <ChartLine data={data} />
+//  * ```
+//  * @return {ReactElement} The rendered line chart component.
+//  */
+export default function ChartLine({ title, ...props }) {
+    const data = {
         x: [1, 2, 3, 4, 5, 6, 7],
         ys: [
             [1, 2, 3, 4, 5, 6, 7],
@@ -21,17 +29,13 @@ import { Typography } from '@mui/material';
             [10, 2, 3, 4, 5, 6, 7],
         ]
     }
-    <ChartLine data={data} />
- * ```
- * @return {ReactElement} The rendered line chart component.
- */
-export default function ChartLine({ data, title, ...props }) {
+
     function resolveTitle() {
         if (title) return <Typography style={{ marginLeft: '16px' }} variant='h5'>{title}</Typography> // title
         return <Typography style={{ marginLeft: '16px' }} variant='h5'>Функция выживаемости</Typography>
     }
-
-    if (!data.ys) return (
+    console.log("hui", data)
+    if (data.ys===undefined) return (
         <>
             <LineChart
                 xAxis={[{ data: data.x }]}
@@ -43,7 +47,7 @@ export default function ChartLine({ data, title, ...props }) {
             />
         </>
     );
-
+    
     if (Array.isArray(data.ys)) return <>
         <LineChart
             height={300}
